b140744d4857b0f57388da888a6212fc2aa77aa1
pywd <yuki.wd7@gmail.com>
2019-09-11 00:17:29 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+	garbage "github.com/yuki-wd/kawasaki-garbage"
+)
+
+func main() {
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.HandleFunc("/job", func(w http.ResponseWriter, req *http.Request) {
+		if req.Header.Get("X-Appengine-Cron") == "true" {
+			tomorrowWeekday := time.Now().AddDate(0, 0, 1).Weekday()
+
+			garbageInfos := garbage.GetGarbageType(tomorrowWeekday)
+			messages := []linebot.SendingMessage{}
+			if len(garbageInfos) > 0 {
+				for info := range garbageInfos {
+					fmt.Printf(garbage.GarbageType(info).String())
+					messages = append(
+						messages,
+						linebot.NewTextMessage(fmt.Sprintf("明日は%vの日だよ！", garbage.GarbageType(info))),
+					)
+				}
+			} else {
+				messages = append(messages, linebot.NewTextMessage("明日のゴミは無いよ！"))
+			}
+			_, err := bot.PushMessage(os.Getenv("GROUP_ID"), messages...).Do()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	})
+
+	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
